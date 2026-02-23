f32a1a083fdd83ce8a99be62a1c18610bbbed7c6
AbbyRead <77890127+AbbyRead@users.noreply.github.com>
2026-02-22 19:53:57 -0800

    Enable sneak fall-off protection
    I had to play around with this a lot to get it to look right and still prevent fall off when holding sneak.  I'm sort of just going in blind and trying to fiddle with things until it looks right.  I think the collision box y level was messing it up before, but when I put it to 0, it sank me into the floor.  So I ended up doing way more than might even be necessary.  Because truth be told, I don't know what I'm doing.  But this worked for me finally, so maybe it helps?  I do like there being a dedicated sneak anim.  👍

--- sbo_crouching/init.lua
+++ sbo_crouching/init.lua
@@ -1,22 +1,22 @@
-minetest.register_globalstep(function(dtime)
-    for _, player in ipairs(minetest.get_connected_players()) do
+core.register_globalstep(function(dtime)
+    for _, player in ipairs(core.get_connected_players()) do
         local controls = player:get_player_control()
 
         if controls.sneak then
             player:set_properties({
-                eye_height = 0.8,
-                visual_size = {x = 1, y = 0.82},
-                collisionbox = { -0.3,-0.6, -0.3, 0.3, 1.4, 0.3 }
+                eye_height = 1.2,
+                visual_size = {x = 1, y = 0.85},
+                collisionbox = { -0.3, 0.0, -0.3, 0.3, 1.4, 0.3 }
             })
-            player:set_bone_position("Body", {x = 0, y = 0, z = 0}, {x = -10, y = 180, z = 0})
+            player:set_bone_position("Body", {x = 0, y = 6.3, z = 0}, {x = -10, y = 180, z = 0})
             player:set_bone_position("Head", {x = 0, y = 6.3, z = 0}, {x = 15, y = 0, z = 0})
         else
             player:set_properties({
-                visual_size = {x = 1, y = 1},
-                eye_height = 1,
-                collisionbox = { -0.3,-0.6, -0.3, 0.3, 1.2, 0.3 }
+                eye_height = 1.7,
+                visual_size = {x = 1, y = 1.05},
+                collisionbox = { -0.3, 0.0, -0.3, 0.3, 1.2, 0.3 }
             })
-            player:set_bone_position("Body", {x = 0, y = 0, z = 0}, {x = 0, y = 180, z = 0})
+            player:set_bone_position("Body", {x = 0, y = 6.3, z = 0}, {x = 0, y = 180, z = 0})
             player:set_bone_position("Head", {x = 0, y = 6.3, z = 0}, {x = 0, y = 0, z = 0})
         end
     end
